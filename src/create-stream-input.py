eb43a025c62396e775fdc33ff234e0db044e1a7c
wangzqi <43793096+wangzqi@users.noreply.github.com>
2020-02-08 22:38:07 -0500

    Update create-stream-input.py

--- src/create-stream-input.py
+++ src/create-stream-input.py
@@ -1,20 +1,14 @@
-import boto3
-import csv
-import json
+mport boto3
 import pandas as pd
-from boto3.dynamodb.conditions import Key, Attr
 import math
 import time
 from datetime import datetime, timedelta
-#import s3fs
-import os,sys
+
 kinesis = boto3.client('kinesis', region_name='us-east-1')
-
-import boto3
-
-client = boto3.client('s3') 
-resource = boto3.resource('s3') 
-mybucket = resource.Bucket('stockalertinput') 
+client = boto3.client('s3')
+resource = boto3.resource('s3')
+mybucket = resource.Bucket('stockalertinput')
+NumberOfShard = 30
 
 def StreamInput():
 
@@ -23,30 +17,33 @@ def StreamInput():
 
     EC2Timedelta = timedelta(hours=19)  # 19 hours is the time difference between EC2 and U.S. East timezone
     x2 = datetime.now()+EC2Timedelta
+
+    # For testing and demo purpose, we will use data starting from 10am if stock market is closed.
     if((x2.strftime("%H:%M:%S")>"16:00:00") or (x2.strftime("%H:%M:%S")<"09:30:00")):
         FileTime = datetime.strptime(datetime.now().strftime("%Y/%m/%d")+" 10:00:00","%Y/%m/%d %H:%M:%S")
         FileTimedelta=FileTime-datetime.now()-EC2Timedelta
-    else:    
+    else:
         FileTimedelta= timedelta(seconds=0)
-    FileTime = datetime.now()+EC2Timedelta+FileTimedelta 
-#    print(FileTime,datetime.now()+EC2Timedelta)
+    FileTime = datetime.now()+EC2Timedelta+FileTimedelta
 
     while True:
-#    for i in range(1):
-        input = client.get_object(Bucket='stockalertinput', Key='Stocks_5s_sample_rate_'+FileTime.strftime("%H%M%S")+'.csv')
-        input1 = pd.read_csv(input['Body'])
-        Data = input1.to_json()
-#        print(Data)
-
+        s3file = client.get_object(Bucket='stockalertinput', Key='Stocks_5s_sample_rate_'+FileTime.strftime("%H%M%S")+'.csv')
+        input = pd.read_csv(s3file['Body'])
         while FileTime > datetime.now()+EC2Timedelta+FileTimedelta:
             time.sleep(1)
         print("Stock Market Time: ",FileTime," Time Now: ",datetime.now()+EC2Timedelta)
-        response = kinesis.put_records(
-            Records=[{'Data': Data,'PartitionKey': 'string'},],
-            StreamName='RI1_fanout'
-            )
-        FileTime = FileTime + timedelta(seconds=5)
 
+        record_per_shard = math.ceil(input['Stock'].count()/NumberOfShard)
+        for i in range(NumberOfShard):
+            rowstart = i*record_per_shard
+            rowend = (i+1)*record_per_shard
+            Data = input[rowstart:rowend].to_json()
+            partitionkey = str(int(i*(2**128/NumberOfShard)))
+            response = kinesis.put_records(
+                    Records=[{'Data': Data,'ExplicitHashKey': partitionkey, 'PartitionKey':'partitionkey'},],
+                    StreamName='RI30_fanout'
+                )
+        FileTime = FileTime + timedelta(seconds=5)
 
 def main():
     StreamInput()
